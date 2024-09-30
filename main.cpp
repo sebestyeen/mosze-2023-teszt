27ce1555c53a3c841c0d86dbe151c00031f5e065
sebestyeen <pungor.sebestyen@gmail.com>
2024-09-30 12:23:37 +0200

    comment bugs

--- main.cpp
+++ main.cpp
@@ -4,20 +4,28 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
+    //alulvonas hianyzik
     int *b = new int[NELEMENTS];
+    //sor nincs lezarva, "
     std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    //for ciklus nincs meghatarozva meddig menjen
+    for (int i = 0)
     {
+        //(i+1)
         b[i] = i * 2;
     }
+    //fura for ciklus
     for (int i = 0; i; i++)
     {
+        //cout fele hianyzik
         std::cout << "Ertek:"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
+    //kezdoertek
     int atlag;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
+        //sor vege
         atlag += b[i]
     }
     atlag /= N_ELEMENTS;
